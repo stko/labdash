4c8929aa3d0f818af9566e5db09e9cb47feebe34
Steffen Koehler <steffen.koehler@mt-technology.de>
2025-02-07 14:44:09 +0100

    updated interface with some documentation

--- labdash/ldmodule.py
+++ labdash/ldmodule.py
@@ -10,8 +10,44 @@ class LDModule(metaclass=ABCMeta):
     def name(self):
         return self._name
 
+
     @abstractmethod
-    def flash(self,start,length,data:bytearray):
+    def hardware_ok():
+        """
+        returns true if the software decide that it's capable
+        to handle the connected module
+        """
+        pass
+    
+    
+    @abstractmethod
+    def flash(self,url:str,flashing_process_indicator:function):
+        """
+        url: URL for download
+        callback function
+        flashing_process_indicator(progress_rate:int)
+        progress_rate: Percentage 0-100
+        
+        In case of error, it raised a FLASH_ERROR exception
+        
+        """
+        pass
+
+    def parameterizing(self,tcpc:dict,parameter_template: dict,external_values: dict, parameter_set_selection:function):
+        """
+        tcpc: standarized vehicle option information dictionary
+        parameter_template: the parameters of that module as template 
+        external_values: additional information like VIN or date time
+        
+        callback function:
+        parameter_set_selection(parameter_selection_list): selection
+        The callback is called with a list of names to choose the wanted
+        parameter set out of it.
+        It returns the selected name
+        
+        In case of error, it raised a FLASH_PARAMETER exception
+        
+        """
         pass
 
     @abstractmethod
