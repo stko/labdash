ac67a0686d64618f498cb7594ec451400282d5c4
Steffen Köhler <steffen.koehler@mt-technology.de>
2025-02-22 12:30:20 +0000

    missing packages added to setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@ from setuptools import setup
 
 setup(
     name='labdash',
-    version='0.0.1',
+    version='0.0.2',
     description='Generic Diagnostic Program with Web UI',
     license='LGPL',
     packages=['labdash'],
     install_requires=[
         'python-can',
         'oyaml',
-        'bitstring'
+        'bitstring',
+	'flask',
+	'flask-sockets',
+	'lxml',
+	'python-can',
+	'canopen'
     ],
     author='Steffen Köhler',
     author_email='steffen@koehlers.de',
