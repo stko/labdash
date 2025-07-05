f49cc17d8071cff3fef0e830ef6ed19eb52c7cbf
Steffen Koehler <steffen.koehler@mt-technology.de>
2025-07-05 14:22:24 +0200

    add more required modules to the setup

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 from setuptools import setup
 
 setup(
-    name='labdash',
-    version='0.0.2',
-    description='Generic Diagnostic Program with Web UI',
-    license='LGPL',
-    packages=['labdash'],
+    name="labdash",
+    version="0.0.2",
+    description="Generic Diagnostic Program with Web UI",
+    license="LGPL",
+    packages=["labdash"],
     install_requires=[
-        'python-can',
-        'oyaml',
-        'bitstring',
-	'flask',
-	'flask-sockets',
-	'lxml',
-	'python-can',
-	'canopen'
+        "python-can",
+        "oyaml",
+        "bitstring",
+        "flask",
+        "flask-sockets",
+        "lxml",
+        "python-can",
+        "canopen",
+        "rich",
+        "requests",
     ],
-    author='Steffen Köhler',
-    author_email='steffen@koehlers.de',
-    keywords=['canbus',"diagnostics","webUI"],
-    url='https://github.com/stko/labdash'
+    author="Steffen Köhler",
+    author_email="steffen@koehlers.de",
+    keywords=["canbus", "diagnostics", "webUI"],
+    url="https://github.com/stko/labdash",
 )
