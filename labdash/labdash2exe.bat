8eafc8f23fb0bf9212d4e4407d9cf8514642926b
Steffen Koehler <steffen.koehler@mt-technology.de>
2025-08-14 11:09:02 +0200

    first attempt to use pyinstaller for a executable binary

---
+++ labdash/labdash2exe.bat
@@ -0,0 +1,24 @@
+pyinstaller labdash.py ^
+--log-level WARN ^
+--collect-submodules labdash ^
+--hiddenimport bitarray ^
+--hiddenimport uuid ^
+--hiddenimport bitstring ^
+--collect-submodules canopen ^
+--collect-submodules flask ^
+--hiddenimport  flask_sockets ^
+--hiddenimport lxml ^
+--hiddenimport oyaml ^
+--collect-submodules python-can ^
+--collect-submodules python-lin ^
+--collect-submodules python-uds ^
+--collect-submodules requests ^
+--hiddenimport rich ^
+--hiddenimport xmltodict 
+mkdir dist\labdash\_internal\labdash\plugins
+mkdir dist\labdash\_internal\web
+xcopy plugins  dist\labdash\_internal\labdash\plugins /s /y
+xcopy ..\web\  dist\labdash\_internal\web /s /y
+xcopy ..\..\..\gitlab\inhouse\labdash_internal  dist\labdash\_internal\web\examples /s /y
+rem del /s /f /q dist\labdash\_internal\labdash\plugins\labdash\__pycache__
+
