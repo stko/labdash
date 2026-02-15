e1cbca03578313c427b881026f2560ea2fed3c66
Steffen Köhler <steffen@koehlers.de>
2026-02-15 14:03:14 +0100

    Faulty, not working Captive portal; optional copy of company internal jonbs and themes

--- ansible/files/hotspot.sh
+++
@@ -1,5 +0,0 @@
-nmcli radio wifi on
-sleep 2
-nmcli device wifi hotspot ssid LabDash password labdashlabdash ifname wlan0
-sleep 2
-nmcli connection modify Hotspot connection.autoconnect yes connection.autoconnect-priority 100
---
+++ ansible/files/server.ts
@@ -0,0 +1,49 @@
+import express, { NextFunction, Request, Response } from 'express';
+import path from 'path';
+import { pong } from './pong';
+
+
+////////////////////////////// Setup ///////////////////////////////////////////
+
+const HOST_NAME = 'rpi-z-labdash';
+const FRONTEND_FOLDER = path.join(__dirname, '../', 'public');
+
+const app = express();
+
+// Redirect every request to our application
+// https://raspberrypi.stackexchange.com/a/100118
+// [You need a self-signed certificate if you really want 
+// an https connection. In my experience, this is just a pain to do
+// and probably overkill for a project where you have your own WiFi network
+// without Internet access anyway.]
+app.use((req: Request, res: Response, next: NextFunction) => {
+    if (req.hostname != HOST_NAME) {
+        return res.redirect(`http://${HOST_NAME}:8000`);
+    }
+    next();
+});
+
+// Call this AFTER app.use where we do the redirects
+app.use(express.static(FRONTEND_FOLDER));
+
+
+/////////////////////////////// Endpoints //////////////////////////////////////
+
+// Serve frontend
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(FRONTEND_FOLDER, 'index.html'));
+});
+
+app.get('/api/ping', pong);
+
+
+///////////////////////////// Server listening /////////////////////////////////
+
+// Listen for requests
+// If you change the port here, you have to adjust the ip tables as well
+// see file: access-point/setup-access-point.sh
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Node version: ${process.version}`);
+    console.log(`⚡ Raspberry Pi Server listening on port ${PORT}`);
+});
